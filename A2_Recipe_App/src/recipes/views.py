05d9b5d8d21cc4ad38be5e9a5d6ffc891d67f54a
Josh Endemann <jahend44@gmail.com>
2023-05-07 16:30:18 -0500

    Added Authentication and CSS

--- A2_Recipe_App/src/recipes/views.py
+++ A2_Recipe_App/src/recipes/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render    # imported by default
 from django.views.generic import ListView, DetailView
 # This will access Recipe model
 from .models import Recipe
+# to protect class-based views
+from django.contrib.auth.mixins import LoginRequiredMixin
+# to protect function-based views
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -13,13 +17,22 @@ def home(request):
 
 # Listview package is a class-based view used to display a list of objects
 
-class RecipeListView(ListView):  # class-based view
+class RecipeListView(LoginRequiredMixin, ListView):  # class-based view
     model = Recipe  # specify model
     template_name = 'recipes/main.html'  # specify template
 
 # Detailview is a class-based view used to display data details
 
 
-class RecipeDetailView(DetailView):  # class-based view
+class RecipeDetailView(LoginRequiredMixin, DetailView):  # class-based view
     model = Recipe  # specify model
     template_name = 'recipes/detail.html'  # specify template
+
+# Define function-based view - records(records()
+# KEEP PROTECTED   -   @login_required
+
+
+@login_required
+def records(request):
+    # do nothing, simply display page
+    return render(request, 'recipes/records.html')
